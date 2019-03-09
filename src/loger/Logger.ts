10499053d01d6cdabc9035407f6fb41e9c39362f
Joseph Lyons <jclyons52@gmail.com>
2019-03-10 10:43:43 +1100

    fixed logging

--- src/loger/Logger.ts
+++ src/loger/Logger.ts
@@ -4,6 +4,6 @@ import { Service } from "typedi";
 export class Logger {
   public log(..._: Array<unknown>) {
     // tslint:disable-next-line: no-console
-    console.log(arguments);
+    console.log(...arguments);
   }
 }
